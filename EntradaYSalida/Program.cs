45aee80321ffb9a6718ea31c942836154e96ae42
Medina21m <99547954+Medina21m@users.noreply.github.com>
2022-02-25 22:59:39 -0600

    Add files via upload

---
+++ EntradaYSalida/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EntradaYSalida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            
+            Console.WriteLine("Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola "+ nombre);
+            Console.Read();
+            
+        }
+    }
+}
